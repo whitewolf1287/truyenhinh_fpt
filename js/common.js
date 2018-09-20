be7c15e6c1428b9b555abec276d938f67bd78f71
whitewolf1287 <baphuoc.tran@gmail.com>
2018-09-20 17:48:03 +0700

    update

--- js/common.js
+++ js/common.js
@@ -99,48 +99,7 @@ $( document ).ready(function() {
 	  }
 	);
 	
-	var swiper = new Swiper('.swiper-container.carousel', {
-        pagination: '.carousel-pagination',
-        paginationClickable: true,
-        spaceBetween: 30,
-		effect: 'fade',
-		loop: true,
-    });
-	var swiper2 = new Swiper('.swiper-container.carousel_gioithieu', {
-       
-        paginationClickable: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 30,
-		effect: 'fade',
-		loop: true,
-    });
-	var swiper3 = new Swiper('.swiper-container.carousel_thethao', {
-        autoplay:5000,
-		effect: 'fade',
-		loop: true,
-    });
-	var swiper4 = new Swiper('.swiper-container.carousel_truyenhinh', {
-        autoplay:5000,
-		effect: 'fade',
-		loop: true,
-    });
-	var swiper5 = new Swiper('.swiper-container.carousel_game_show', {
-        autoplay:5000,
-		effect: 'fade',
-		loop: true,
-    });
-	var swiper6 = new Swiper('.swiper-container.carousel_sukien', {
-        autoplay:5000,
-		effect: 'fade',
-		loop: true,
-    });
-	var swiper7 = new Swiper('.swiper-container.carousel_phimtruyen', {
-        autoplay:5000,
-		effect: 'fade',
-		loop: true,
-    });
-	
+
 	
 	
 });
