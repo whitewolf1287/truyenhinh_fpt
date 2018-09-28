30f95a847b0ce39c2c9957ddc90214f5ee256c24
whitewolf1287 <baphuoc.tran@gmail.com>
2018-09-28 10:53:46 +0700

    fix bug

--- js/common.js
+++ js/common.js
@@ -86,6 +86,7 @@ $( document ).ready(function() {
 		}
 		else{
 			$(".HeaderPage").removeClass("scaleheader");
+			$('.par').removeClass('disable');
 			$('.title').text($('.navigation').html());
 		}
 	});
